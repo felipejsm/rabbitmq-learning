5cfd751ce9b078dc97d076e862b90c6747f1f754
felipejsm <felipejsm41@gmail.com>
2020-04-18 23:41:31 -0300

    correcoes

--- topic/ReceiveLogsTopic.java
+++ topic/ReceiveLogsTopic.java
@@ -3,33 +3,34 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.DeliverCallback;
 
-public class ReceiveLogsTopic  {
-	public static final String EXCHANGE_NAME ="topic_logs";
-	
-	public static void main(String[] argv) throws Exception {
-		ConnectionFactory connectionFactory = new ConnectionFactory();
-		connectionFactory.setHost("localhost");
-		
-		Connection connection = connectionFactory.newConnection();
-		Channel channel = connection.createChannel();
-		
-		channel.exchangeDeclare(EXCHANGE_NAME, "topic");
-		
-		String queueName = channel.queueDeclare().getQueue();
-		
-		if (argv.length < 1) {
-			System.err.println("Usage: ReceiveLogsTopic [binding_key]...");
-			System.exit(1);
-		}
-		 System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-		 
-		 DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-			 String msg = new String(delivery.getBody(),"UTF-8");
-			 System.out.println(" [x] Received '" +
-			  delivery.getEnvelope().getRoutingKey() + "':'" + msg + "'");
-			 
-		 };
-		 channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {});
-		
-	}
+public class ReceiveLogsTopic {
+
+    private static final String EXCHANGE_NAME = "topic_logs";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "topic");
+        String queueName = channel.queueDeclare().getQueue();
+
+        if (argv.length < 1) {
+            System.err.println("Usage: ReceiveLogsTopic [binding_key]...");
+            System.exit(1);
+        }
+
+        for (String bindingKey : argv) {
+            channel.queueBind(queueName, EXCHANGE_NAME, bindingKey);
+        }
+
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + delivery.getEnvelope().getRoutingKey() + "':'" + message + "'");
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
+    }
 }
